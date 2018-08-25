aa1e9553bddc34c7db7e5dbf37fc3c5d1813bbaa
unknown <laura.feregotti@gmail.com>
2018-08-25 01:09:10 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,809 @@
+$(document).ready(function() {
+
+ /* DESPLEGABLE*/ 
+
+$('#desplegable').hide()
+  $('#desplegar').hover(function(){
+           $('#desplegable').show().delay( 2000 ).hide( 0 );
+           
+ 
+              
+              
+              });
+
+
+
+  /* CARRERA*/ 
+
+$('#click9').hide()
+   $('#click8').hide()
+   $('#click7').hide()
+      $('#click6').hide()
+         $('#click5').hide()
+            $('#click4').hide()
+               $('#click3').hide()
+                  $('#click2').hide()
+                     $('#click1').show()
+
+
+
+
+
+
+
+
+     $('#contenido9').hide()
+   $('#contenido8').hide()
+   $('#contenido7').hide()
+   $('#contenido6').hide()
+   $('#contenido5').hide()
+  $('#contenido2').hide();
+  $('#contenido3').hide()
+    $('#contenido4').hide()
+        
+    $('#1943').click(function(){
+           $('#contenido2').show()
+            $('#contenido').hide()
+            $('#contenido3').hide()
+               $('#contenido4').hide()
+                 $('#contenido5').hide()
+                 $('#contenido6').hide()
+                  $('#contenido7').hide()
+                    $('#contenido9').hide()
+                     $('#click1').hide()
+                      $('#click2').show()
+                      $('#click3').hide()
+                      $('#click4').hide()
+                      $('#click5').hide()
+                       $('#click6').hide()
+                       $('#click7').hide()
+                          $('#click8').hide()
+                           $('#click9').hide()                 
+         });                
+                        
+ $('#1935').click(function(){
+           $('#contenido2').hide()
+           $('#contenido3').hide()
+            $('#contenido').show()
+               $('#contenido4').hide()
+                 $('#contenido5').hide()
+                 $('#contenido6').hide()
+                  $('#contenido7').hide()
+                    $('#contenido8').hide()
+                      $('#contenido9').hide()
+                       $('#click1').show()
+                        $('#click2').hide()
+                        $('#click3').hide()
+                      $('#click4').hide()
+                      $('#click5').hide()
+                       $('#click6').hide()
+                       $('#click7').hide()
+                          $('#click8').hide()
+                           $('#click9').hide()  
+         });     
+     $('#1948').click(function(){
+           $('#contenido2').hide()
+            $('#contenido').hide()
+              $('#contenido3').show()
+               $('#contenido4').hide()
+     $('#contenido5').hide()
+     $('#contenido6').hide()
+      $('#contenido7').hide()
+        $('#contenido8').hide()
+          $('#contenido9').hide()
+   $('#click1').hide()
+                      $('#click2').hide()
+                      $('#click3').show()
+                      $('#click4').hide()
+                      $('#click5').hide()
+                       $('#click6').hide()
+                       $('#click7').hide()
+                          $('#click8').hide()
+                           $('#click9').hide()  
+
+         });  
+   
+         $('#1949').click(function(){
+           $('#contenido2').hide()
+            $('#contenido').hide()
+              $('#contenido3').hide()
+   $('#contenido4').show()
+    $('#contenido5').hide()
+    $('#contenido6').hide()
+     $('#contenido7').hide()
+       $('#contenido8').hide()
+         $('#contenido9').hide()
+   $('#click1').hide()
+                      $('#click2').hide()
+                      $('#click3').hide()
+                      $('#click4').show()
+                      $('#click5').hide()
+                       $('#click6').hide()
+                       $('#click7').hide()
+                          $('#click8').hide()
+                           $('#click9').hide()  
+
+
+
+         });  
+    
+         $('#1970').click(function(){
+           $('#contenido2').hide()
+            $('#contenido').hide()
+              $('#contenido3').hide()
+   $('#contenido4').hide()
+      $('#contenido5').show()
+      $('#contenido6').hide()
+       $('#contenido7').hide()
+         $('#contenido8').hide()
+           $('#contenido9').hide()
+   $('#click1').hide()
+                      $('#click2').hide()
+                      $('#click3').hide()
+                      $('#click4').hide()
+                      $('#click5').show()
+                       $('#click6').hide()
+                       $('#click7').hide()
+                          $('#click8').hide()
+                           $('#click9').hide()  
+
+
+         });  
+     
+         $('#1972').click(function(){
+           $('#contenido2').hide()
+            $('#contenido').hide()
+              $('#contenido3').hide()
+   $('#contenido4').hide()
+      $('#contenido5').hide()
+      $('#contenido6').show()
+       $('#contenido7').hide()
+         $('#contenido8').hide()
+           $('#contenido9').hide()
+   $('#click1').hide()
+                      $('#click2').hide()
+                      $('#click3').hide()
+                      $('#click4').hide()
+                      $('#click5').hide()
+                       $('#click6').show()
+                       $('#click7').hide()
+                          $('#click8').hide()
+                           $('#click9').hide()  
+
+
+
+
+         });   
+    
+         
+         $('#1979').click(function(){
+           $('#contenido2').hide()
+           $('#contenido3').hide()
+            $('#contenido').hide()
+               $('#contenido4').hide()
+                 $('#contenido5').hide()
+                 $('#contenido6').hide()
+                   $('#contenido7').show()
+                     $('#contenido8').hide()
+                       $('#contenido9').hide()
+   $('#click1').hide()
+                      $('#click2').hide()
+                      $('#click3').hide()
+                      $('#click4').hide()
+                      $('#click5').hide()
+                       $('#click6').hide()
+                       $('#click7').show()
+                          $('#click8').hide()
+                           $('#click9').hide()  
+
+
+
+                   });
+
+         $('#1998').click(function(){
+           $('#contenido2').hide()
+           $('#contenido3').hide()
+            $('#contenido').hide()
+               $('#contenido4').hide()
+                 $('#contenido5').hide()
+                 $('#contenido6').hide()
+                   $('#contenido7').hide()
+                     $('#contenido8').show()
+                       $('#contenido9').hide()
+   $('#click1').hide()
+                      $('#click2').hide()
+                      $('#click3').hide()
+                      $('#click4').hide()
+                      $('#click5').hide()
+                       $('#click6').hide()
+                       $('#click7').hide()
+                          $('#click8').show()
+                           $('#click9').hide()  
+
+
+
+                   });
+
+         $('#2012').click(function(){
+           $('#contenido2').hide()
+           $('#contenido3').hide()
+            $('#contenido').hide()
+               $('#contenido4').hide()
+                 $('#contenido5').hide()
+                 $('#contenido6').hide()
+                   $('#contenido7').hide()
+                     $('#contenido8').hide()
+                       $('#contenido9').show()
+   $('#click1').hide()
+                      $('#click2').hide()
+                      $('#click3').hide()
+                      $('#click4').hide()
+                      $('#click5').hide()
+                       $('#click6').hide()
+                       $('#click7').hide()
+                          $('#click8').hide()
+                           $('#click9').show()  
+
+
+
+                   });
+
+
+
+   /* SOUNDTRACK*/
+   $('#botonn1').show()
+$('#botonn2').hide()
+$('#botonn3').hide()
+$('#botonn4').hide()
+$('#botonn5').hide()
+$('#botonn6').hide()
+$('#botonn7').hide()
+$('#botonn8').hide()
+$('#botonn9').hide()
+$('#botonn10').hide()
+
+
+  $('#soundtrack1').show()
+   $('#soundtrack2').hide()
+   $('#soundtrack3').hide()
+   $('#soundtrack4').hide()
+   $('#soundtrack5').hide()
+  $('#soundtrack6').hide();
+  
+
+
+         $('#bullets').click(function(){
+           $('#soundtrack1').show()
+            $('#soundtrack2').hide()
+             $('#soundtrack3').hide()
+             $('#soundtrack4').hide()
+              $('#soundtrack5').hide()
+               $('#soundtrack6').hide();
+                  
+$('#botonn1').hide()
+$('#botonn2').hide()
+$('#botonn3').hide()
+$('#botonn4').hide()
+$('#botonn5').hide()
+$('#botonn6').show()
+
+              
+              
+              });
+
+$('#man').click(function(){
+           $('#soundtrack1').hide()
+            $('#soundtrack2').show()
+             $('#soundtrack3').hide()
+             $('#soundtrack4').hide()
+              $('#soundtrack5').hide()
+               $('#soundtrack6').hide();
+                  
+$('#botonn1').hide()
+$('#botonn2').hide()
+$('#botonn3').hide()
+$('#botonn4').hide()
+$('#botonn5').show()
+$('#botonn6').hide()
+
+              
+              });
+
+                   $('#sweet').click(function(){
+           $('#soundtrack1').hide()
+            $('#soundtrack2').hide()
+             $('#soundtrack3').show()
+             $('#soundtrack4').hide()
+              $('#soundtrack5').hide()
+               $('#soundtrack6').hide();
+                  
+$('#botonn1').hide()
+$('#botonn2').hide()
+$('#botonn3').hide()
+$('#botonn4').show()
+$('#botonn5').hide()
+$('#botonn6').hide()
+
+              
+              
+              });
+
+
+      $('#stardust').click(function(){
+           $('#soundtrack1').hide()
+            $('#soundtrack2').hide()
+             $('#soundtrack3').hide()
+             $('#soundtrack4').show()
+              $('#soundtrack5').hide()
+               $('#soundtrack6').hide();
+
+$('#botonn1').hide()
+$('#botonn2').hide()
+$('#botonn3').show()
+$('#botonn4').hide()
+$('#botonn5').hide()
+$('#botonn6').hide()
+
+              
+              });
+
+$('#media').click(function(){
+           $('#soundtrack1').hide()
+            $('#soundtrack2').hide()
+             $('#soundtrack3').hide()
+             $('#soundtrack4').hide()
+              $('#soundtrack5').show()
+               $('#soundtrack6').hide();
+
+$('#botonn1').hide()
+$('#botonn2').show()
+$('#botonn3').hide()
+$('#botonn4').hide()
+$('#botonn5').hide()
+$('#botonn6').hide()
+
+
+
+                   });
+$('#every').click(function(){
+           $('#soundtrack1').hide()
+            $('#soundtrack2').hide()
+             $('#soundtrack3').hide()
+             $('#soundtrack4').hide()
+              $('#soundtrack5').hide()
+               $('#soundtrack6').show();
+
+
+$('#botonn1').show()
+$('#botonn2').hide()
+$('#botonn3').hide()
+$('#botonn4').hide()
+$('#botonn5').hide()
+$('#botonn6').hide()
+
+
+
+
+
+                   });
+
+
+  /* DISCOS*/ 
+
+
+$('#disco2').hide()
+$('#disco1').show()
+
+$('#wildpeque').click(function(){
+           $('#disco2').show()
+            $('#disco1').hide()
+
+            
+            
+                   });
+
+
+$('#manmed').click(function(){
+           $('#disco1').show()
+            $('#disco2').hide()
+            
+                   });
+
+/* CLASICOS*/ 
+
+$('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').hide()
+
+
+
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').hide()
+$('#clasico10').show()
+  
+  
+
+$('#beet').hover(function(){
+
+$('#clasico10').show()
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').hide()
+
+$('#boton1').show()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').hide()
+            
+                   });
+
+
+$('#tcha').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').show()
+
+$('#boton1').hide()
+$('#boton2').show()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').hide()
+            
+                   });
+
+$('#puccini').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').show()
+$('#clasico9').hide()
+
+
+$('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').show()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').hide()
+            
+                   });
+
+$('#bizet').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').show()
+$('#clasico8').hide()
+$('#clasico9').hide()
+
+            
+$('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').show()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').hide()
+
+                   });
+
+$('#albeniz').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').show()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').hide()
+
+
+
+$('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').show()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').hide()
+            
+                   });
+
+$('#bach').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').show()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').hide()
+
+$('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').show()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').hide()
+
+            
+                   });
+
+$('#gershwin').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').show()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').hide()
+
+ $('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').show()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').hide()
+
+                   });
+
+
+$('#wagner').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').hide()
+$('#clasico2').hide()
+$('#clasico3').show()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').hide()
+
+$('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').show()
+$('#boton9').hide()
+$('#boton10').hide()
+
+            
+                   });
+
+$('#strauss').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').hide()
+$('#clasico2').show()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').hide()
+
+
+$('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').show()
+$('#boton10').hide()
+            
+                   });
+
+$('#stravinsky').hover(function(){
+
+$('#clasico10').hide()
+$('#clasico1').show()
+$('#clasico2').hide()
+$('#clasico3').hide()
+$('#clasico4').hide()
+$('#clasico5').hide()
+$('#clasico6').hide()
+$('#clasico7').hide()
+$('#clasico8').hide()
+$('#clasico9').hide()
+
+$('#boton1').hide()
+$('#boton2').hide()
+$('#boton3').hide()
+$('#boton4').hide()
+$('#boton5').hide()
+$('#boton6').hide()
+$('#boton7').hide()
+$('#boton8').hide()
+$('#boton9').hide()
+$('#boton10').show()
+            
+                   });
+
+/* JAZZ*/ 
+
+
+$('#jazz1').show()
+$('#jazz2').hide()
+$('#jazz3').hide()
+
+$('#primero').hide()
+$('#segundo').hide()
+$('#tercero').hide()
+
+
+
+$('#jazzilli').hide()
+$('#jazzloui').show()
+$('#jazznew').hide()
+
+
+$('#1910').hover(function(){
+$('#jazzloui').show()
+$('#jazzilli').hide()
+$('#jazznew').hide()
+$('#primero').show()
+$('#segundo').hide()
+$('#tercero').hide()
+                   });
+
+
+$('#1920').hover(function(){
+$('#jazzloui').hide()
+$('#jazzilli').show()
+$('#jazznew').hide()
+$('#primero').hide()
+$('#segundo').show()
+$('#tercero').hide()
+
+                   });
+
+
+$('#1930').hover(function(){
+$('#jazzilli').hide()
+$('#jazzloui').hide()
+$('#jazznew').show()
+$('#primero').hide()
+$('#segundo').hide()
+$('#tercero').show()
+                   });
+
+   
+$('#circulo1').hover(function(){
+$('#jazz1').show()
+$('#jazz2').hide()
+$('#jazz3').hide()
+ });
+$('#circulo2').hover(function(){
+$('#jazz1').hide()
+$('#jazz2').show()
+$('#jazz3').hide()
+ });
+$('#circulo3').hover(function(){
+$('#jazz1').hide()
+$('#jazz2').hide()
+$('#jazz3').show()
+ });
+
+
+
+
+
+
+
+
+
+
+
+/* CABEZA*/
+
+
+$('#cabezawoody').animate({ width:'253px',height:'302px'},30000);
+
+/*  BOTONES INICIO*/
+
+$('#cafewoody').hide()
+$('#generoswoody').hide()
+$('#soundtrackwoody').hide()
+$('#carrerawoody').hide()
+$('#discoswoody').hide()
+
+$('#dis').hover(function(){
+
+$('#discoswoody').show().delay( 2000 ).hide( 0 );
+           
+
+});
+
+
+
+
+
+
+});
